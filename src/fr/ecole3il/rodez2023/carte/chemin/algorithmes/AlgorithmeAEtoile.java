6c7bdb6e24566689aad7a7f5550fa6e753c9b670
camillemoutahir <moutahic@3il.fr>
2024-03-26 23:43:42 +0100

    modif code initialisation

--- src/fr/ecole3il/rodez2023/carte/chemin/algorithmes/AlgorithmeAEtoile.java
+++ src/fr/ecole3il/rodez2023/carte/chemin/algorithmes/AlgorithmeAEtoile.java
@@ -1,13 +1,32 @@
 package fr.ecole3il.rodez2023.carte.chemin.algorithmes;
 import fr.ecole3il.rodez2023.carte.chemin.elements.Graphe;
 import fr.ecole3il.rodez2023.carte.chemin.elements.Noeud;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
 
 public class AlgorithmeAEtoile<E> implements AlgorithmeChemin<E> {
+    /*Partie 1 :Initialisation */
      @Override
     public List<Noeud<E>> trouverChemin(Graphe<E> graphe, Noeud<E> depart, Noeud<E> arrivee) {
-        // Algorithme A* ici pour trouver un chemin entre le noeud de départ et le noeud d'arrivée dans le graphe donné
-        // Doit retourner une liste de noeuds représentant le chemin trouvé
+        Map<Noeud<E>, Noeud<E>> listePred = new HashMap<>();
+        Map<Noeud<E>, Double> coutEstime = new HashMap<>();
+        Map<Noeud<E>, Double> coutReel = new HashMap<>();
+
+        for (Noeud<E> noeud : graphe.getNoeuds()){
+            coutEstime.put(noeud, Double.POSITIVE_INFINITY);
+            coutReel.put(noeud, Double.POSITIVE_INFINITY);
+            listePred.put(noeud, null);
+        }
+        coutEstime.put(depart,0.0);
+        coutReel.put(depart,0.0);
+
+        PriorityQueue<Noeud<E>> filePriorite = new PriorityQueue<>(Comparator.comparingDouble(coutEstime::get));
+        filePriorite.add(depart);
+
+
         return null; 
     }
 }
