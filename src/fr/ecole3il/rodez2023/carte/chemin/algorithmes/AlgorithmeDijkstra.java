9ceb8458dff35fe207cfbc82d37c9ecc09119840
camillemoutahir <moutahic@3il.fr>
2024-03-26 22:32:02 +0100

    modif code partie 1 initialisation structures de donnees

--- src/fr/ecole3il/rodez2023/carte/chemin/algorithmes/AlgorithmeDijkstra.java
+++ src/fr/ecole3il/rodez2023/carte/chemin/algorithmes/AlgorithmeDijkstra.java
@@ -1,25 +1,39 @@
 package fr.ecole3il.rodez2023.carte.chemin.algorithmes;
 
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
 
 import fr.ecole3il.rodez2023.carte.chemin.elements.Graphe;
 import fr.ecole3il.rodez2023.carte.chemin.elements.Noeud;
 
 public class AlgorithmeDijkstra <E> implements AlgorithmeChemin<E>{
-    //Initialisation des structures de données
+    /*Partie 1 : Initialisation des structures de données*/
     @Override
     public List<Noeud<E>> trouverChemin(Graphe<E> graphe, Noeud<E> depart, Noeud<E> arrivee) {
-        // Initialisation des structures de données
-        Map<Noeud<E>, Double> couts = new HashMap<>();
-        Map<Noeud<E>, Noeud<E>> predecesseurs = new HashMap<>();
-    // Initialisation des prédecesseurs et ds coûts
-    //File de priorité pour les noeuds à explorer
+        /*Initialisation des structures de données*/
+        Map<Noeud<E>, Double> listeCouts = new HashMap<>();
+        Map<Noeud<E>, Noeud<E>> listePred = new HashMap<>();
+        /**
+         * Initialisation des prédecesseurs et des coûts de chaque noeud
+         * */
+        for(Noeud<E> noeud : graphe.getNoeuds()){
+            //Cout des noeuds (sauf celui de depart) mis à l'infini
+            listeCouts.put(noeud, Double.POSITIVE_INFINITY);
+            //Predecesseurs de chaque noeud initialisés à 0
+            listePred.put(noeud,null);
+        }
+        // Cout du noeud de depart initialisé à 0
+        listeCouts.put(depart,0.0);
 
-    //Exploration des noeuds
+        /*File de priorité qui classe les noeuds à explorer par leur cout actuel*/
+        PriorityQueue<Noeud<E>> filePriorite = new PriorityQueue<>(Comparator.comparingDouble(listeCouts::get));
+    /*Partie 2 : Exploration des noeuds*/
+    
 
-    // Retourne le chemin le plus court trouvé
+    /*Partie 3 : Retourne du chemin le plus court trouvé*/
     return null;
 }
 }
