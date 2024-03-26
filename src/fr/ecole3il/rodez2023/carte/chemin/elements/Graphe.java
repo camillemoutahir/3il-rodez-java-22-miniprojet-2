24a262e8a1f27dcc964e016bb67a0b973ccf6152
camillemoutahir <moutahic@3il.fr>
2024-03-26 21:53:43 +0100

    modification code et javadoc

--- src/fr/ecole3il/rodez2023/carte/chemin/elements/Graphe.java
+++ src/fr/ecole3il/rodez2023/carte/chemin/elements/Graphe.java
@@ -1,6 +1,7 @@
 package fr.ecole3il.rodez2023.carte.chemin.elements;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 /*Question : Quelle structure de données pourrait être utilisée pour stocker les relations entre les nœuds du graphe
@@ -13,12 +14,13 @@ Réponse : On utilise des paramètres génériques comme ça on peut changer leu
  */
 
 public class Graphe <E> {
-    private List<E> graphe;
-    private Map<Noeud<E>, List<Noeud<E>>> adjacence;
+    private Map<Noeud<E>, Map<Noeud<E>, Double>> listeNoeuds;
 
+    /**
+     * Constructueur Graphe qui initialise listeNoeuds
+     */
     public Graphe(){
-        this.graphe = null;
-        this.adjacence = null;
+        this.listeNoeuds = new HashMap<>();
     }
 
     /**
@@ -26,41 +28,40 @@ public class Graphe <E> {
      * @param noeud
      */
     public void ajouterNoeud(Noeud<E> noeud){
-        if (!adjacence.containsKey(noeud)){
-            adjacence.put(noeud, new ArrayList<>());
+        if (!listeNoeuds.containsKey(noeud)){
+            listeNoeuds.put(noeud, new HashMap<>());
         }
     }
-    /*Cette méthode ajoute une arête pondérée entre deux nœuds du graphe. Elle prend en paramètre le nœud de départ, le nœud d'arrivée et le coût de l'arête.
-    Si les nœuds de départ et d'arrivée n'existent pas encore dans le graphe, ils sont ajoutés en utilisant la méthode ajouterNoeud.
-    Ne pas oublier d'initialiser l'arête à la matrice d'adjacence associée au nœud de départ.
-     */
 
     /**
-     * Ajoute une arête pondéré entre 2 noeuds du graphe
+     * Ajoute une arête pondérée entre 2 noeuds du graphe
      * @param depart
      * @param arrivee
-     * @param cout de l'arrete
+     * @param cout de l'arete
      */
     public void ajouterArete(Noeud<E> depart, Noeud<E> arrivee, double cout){
-        double arete =0;
-        if(!adjacence.containsKey(depart)){
+        /*On vérifie si les noeuds de départ et d'arrivee 
+        existent sinon les creer*/
+        if(!listeNoeuds.containsKey(depart)){
             ajouterNoeud(depart);
         }
-        if(!adjacence.containsKey(arrivee)){
+        if(!listeNoeuds.containsKey(arrivee)){
             ajouterNoeud(arrivee);
         }
-        adjacence.get(depart).add(arrivee);
-        //TODO ajouter cout arete
+        listeNoeuds.get(depart).put(arrivee,cout);
     }
 
     /**
      * Renvoie coût de l'arête entre les 2 noeuds spécifiés
-     * @param depart
-     * @param arrivee
-     * @return coût de l'arête entre les 2 noeuds spécifiés
+     * @param depart noeud de départ
+     * @param arrivee noeud d'arrivee
+     * @return coût de l'arête entre les 2 (si un des noeuds n'existent pas renvoie 0)
      */
     public double getCoutArete(Noeud<E> depart, Noeud<E> arrivee){
-        return 0.0;
+        if (listeNoeuds.containsKey(depart)&& listeNoeuds.containsKey(arrivee)){
+            return this.listeNoeuds.get(depart).get(arrivee);
+        }
+        return 0;
     }
 
     /**
@@ -68,15 +69,16 @@ public class Graphe <E> {
      * @return liste contenant les noeuds du graphe
      */
     public List<Noeud<E>> getNoeuds(){
-        return new ArrayList<>(adjacence.keySet());
+        return new ArrayList<>(listeNoeuds.keySet());
     }
     
     /**
      * Renvoie liste contenant tous les voisins d'un noeud spécifié
-     * @param noeud
-     * @return liste contenant tous les voisins du noeud
+     * @param noeud noeud dont on cherche tous les voisins
+     * @return liste contenant tous les voisins de ce noeud
      */
     public List<Noeud<E>> getVoisins(Noeud<E> noeud){
-        return adjacence.getOrDefault(noeud, new ArrayList<>());
+        return noeud.getVoisins();
     }
 }
+
