6307c9a394c63c89effbce58ed570008e48d6645
camillemoutahir <moutahic@3il.fr>
2024-03-28 22:32:36 +0100

    modif

--- src/fr/ecole3il/rodez2023/carte/application/ExempleCLI.java
+++ src/fr/ecole3il/rodez2023/carte/application/ExempleCLI.java
@@ -17,13 +17,13 @@ import fr.ecole3il.rodez2023.carte.manipulateurs.GenerateurCarte;
  */
 public class ExempleCLI {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		GenerateurCarte generateur = new GenerateurCarte();
 		Carte test = generateur.genererCarte(100,100);
 		AlgorithmeChemin algoChemin = new AlgorithmeDijkstra();
-		Chemin chemin = algoChemin.trouverChemin(test, 0, 0, 50, 50);
+		Chemin chemin = AdaptateurAlgorithme.trouverChemin(algoChemin,test, 0, 0, 50, 50);
 		chemin.afficherChemin();
-		chemin = algoChemin.trouverChemin(test, 0, 0, 50, 50);
+		chemin =AdaptateurAlgorithme.trouverChemin(algoChemin,test, 0, 0, 50, 50);
 		chemin.afficherChemin();
 	}
 	
