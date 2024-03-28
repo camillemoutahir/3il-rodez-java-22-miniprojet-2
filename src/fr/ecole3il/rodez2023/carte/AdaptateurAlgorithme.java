0a33755fbebd2ecc31597dff6529e47f2aaaf52c
camillemoutahir <moutahic@3il.fr>
2024-03-28 19:04:50 +0100

    modif methode trouver chemin

--- src/fr/ecole3il/rodez2023/carte/AdaptateurAlgorithme.java
+++ src/fr/ecole3il/rodez2023/carte/AdaptateurAlgorithme.java
@@ -1,5 +1,6 @@
 package fr.ecole3il.rodez2023.carte;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import fr.ecole3il.rodez2023.carte.chemin.algorithmes.AlgorithmeChemin;
@@ -9,24 +10,32 @@ import fr.ecole3il.rodez2023.carte.elements.Carte;
 import fr.ecole3il.rodez2023.carte.elements.Case;
 import fr.ecole3il.rodez2023.carte.elements.Chemin;
 
-public class AdaptateurAlgorithme {/*
+public class AdaptateurAlgorithme {
     public static Chemin trouverChemin(AlgorithmeChemin<Case> algorithme, Carte carte, int xDepart, int yDepart, int xArrivee, int yArrivee){
         Graphe<Case> graphe = creerGraphe(carte);
         Case depart = carte.getCase(xDepart, yDepart);
         Case arrivee = carte.getCase(xArrivee, yArrivee);
 
         List<Noeud<Case>> chemin = algorithme.trouverChemin(graphe, depart, arrivee);
-        return adapterChemin(chemin);
+        List<Case> CaseChemin = new ArrayList<>();
+        for (Noeud<Case> noeud : chemin){
+            CaseChemin.add(noeud.getValeur());
+        }
+        return new Chemin(chemin);
     }
     public static Graphe <Case> creerGraphe(Carte carte){
-
+        Graphe<Case> graphe = new Graphe<Case>();
+        return graphe;
     }
     public static void ajouterAretesVoisines(Graphe<Case> graphe, Case currentCase, int x, int y, int largeur, int hauteur) {
     }
     public static double calculerCout(Case from, Case to){
+        double coutcalcule =0.0;
 
+        return coutcalcule;
     }
-    public static afficherChemin(List<Noeud<Case>> chemin){
+    /*public static afficherChemin(List<Noeud<Case>> chemin){
 
     } */
 }
+
